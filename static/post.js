de5137210519f5f20504880f1f33fbf5386318a0
gravitys <alternativegravitational@protonmail.com>
2023-12-25 17:51:40 +0800

    yeah baby forms

--- static/post.js
+++ static/post.js
@@ -1,12 +1,22 @@
-var editor = ace.edit("ace-editor");
-editor.setTheme("ace/theme/tomorrow_night");
-editor.setOptions({
+var editor = ace.edit("ace-editor", {
 	maxLines: 10,
+	theme: "ace/theme/tomorrow_night",
 });
 
 var language_select = document.getElementById("language");
 language_select.addEventListener("input", function() {
 	var selected = language_select.options[language_select.selectedIndex].value;
 	editor.session.setMode("ace/mode/" + selected);
-	var code = editor.getValue();
 });
+
+var editor_max = document.getElementById("editorMax");
+editor_max.addEventListener("input", function() {
+	editor.session.maxLines(editor_max.value);
+});
+
+var form = document.getElementById("form");
+
+form.onsubmit = () => {
+	var submit_button = document.getElementById("submit_button");
+	submit_button.value = editor.getValue();
+};
