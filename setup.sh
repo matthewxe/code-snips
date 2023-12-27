1fffd0c90e35e8d186da46d670dfb8539b79f677
gravitys <alternativegravitational@protonmail.com>
2023-12-27 22:44:24 +0800

    gone

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
  
 python -m venv ./venv
+npm i --prefix ./static/npm/ bootstrap@5.3.2
 sass ./static/scss/custom.scss ./static/css/bootstrap.css
 source ./venv/bin/activate
 pip install -r requirements.txt
