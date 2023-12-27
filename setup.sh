009f372a8952982de005b5f8896cf9d14ee81264
gravitys <alternativegravitational@protonmail.com>
2023-12-27 22:53:39 +0800

    better

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
  
 python -m venv ./venv
+pip install nodeenv
 nodeenv -p
+source ./venv/bin/activate
 npm i --prefix ./static/npm/ bootstrap@5.3.2
 npm install -g sass
 sass ./static/scss/custom.scss ./static/css/bootstrap.css
-source ./venv/bin/activate
 pip install -r requirements.txt
  
