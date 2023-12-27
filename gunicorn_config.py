84a382e39371c47155817579e8717c18d09d95f4
gravitys <alternativegravitational@protonmail.com>
2023-12-27 21:32:34 +0800

    gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,9 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-workers = int(os.environ.get('GUNICORN_WORKERS', '4'))
-threads = int(os.environ.get('GUNICORN_THREADS', '100'))
+# workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+workers = int(os.environ.get('GUNICORN_WORKERS', '5'))
+# threads = int(os.environ.get('GUNICORN_THREADS', '2'))
+os.environ.get('GUNICORN_WORKER_CLASS', 'gevent')
+os.environ.get('GUNICORN_WORKER_CONNECTIONS', '1000')
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
