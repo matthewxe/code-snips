83f2b1325e3589359301489cdc250402b25ff055
gravitys <alternativegravitational@protonmail.com>
2023-12-27 21:43:25 +0800

    idfk

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source ./venv/bin/activate
-gunicorn -b localhost:8000 --config gunicorn_config.py 'app:app'
+# gunicorn -b localhost:8000 --config gunicorn_config.py app:app
+gunicorn -b localhost:8000 --worker-class=gevent --worker-connections=1000 --workers=3 app:app
