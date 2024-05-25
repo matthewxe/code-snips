afbfeb905b1104e31798d755a576b1b811886d76
Matthew <matthewelento29@gmail.com>
2024-05-25 19:09:09 +0800

    wahhh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,10 @@
  
 python -m venv ./venv
 source ./venv/bin/activate
+pip install --upgrade pip
 pip install nodeenv
 nodeenv -p
-npm i --prefix ./static/npm/ bootstrap@5.3.2
+npm i --prefix ./static/npm/ bootstrap@4.3.2
 npm install -g sass
 sass ./static/scss/custom.scss ./static/css/bootstrap.css
 pip install -r requirements.txt
