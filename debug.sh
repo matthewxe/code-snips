e8ff8ec403333dd5ba621d7e25aed2011036bcf0
Matthew <matthewelento29@gmail.com>
2024-05-25 19:09:09 +0800

    wtf

--- debug.sh
+++ debug.sh
@@ -3,4 +3,4 @@
 source venv/bin/activate
 # pip3 install -r ./requirements.txt
 sass --watch ./static/scss/custom.scss ./static/css/bootstrap.css &
-flask --app app.py --debug run --port:8000 
+flask --app app.py --debug run --port 8000 
