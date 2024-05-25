bb630dcdc13111c1b871cd20ce582836b4c46f69
Matthew <matthewelento29@gmail.com>
2024-05-25 19:09:09 +0800

    arstno

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
  
 python -m venv ./venv
+nodeenv -p
 npm i --prefix ./static/npm/ bootstrap@5.3.2
+npm install -g sass
 sass ./static/scss/custom.scss ./static/css/bootstrap.css
 source ./venv/bin/activate
 pip install -r requirements.txt
