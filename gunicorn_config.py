dc6fc089f56648de8a0852051935604152ebc68b
Matthew <matthewelento29@gmail.com>
2024-05-25 19:09:08 +0800

    yeah

---
+++ gunicorn_config.py
@@ -0,0 +1,6 @@
+import os
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+forwarded_allow_ips = '*'
+secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
